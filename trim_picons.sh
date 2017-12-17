736931f36f8d32c63f8d3a97c301488b0cc50b46
root <root@vps448126.ovh.net>
2017-12-17 14:04:11 +0100

    fixs

--- trim_picons.sh
+++ trim_picons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Triming png picons"
-mogrify -trim  -resize 250x200 -gravity Center -extent 250x200 -path ./logoChannels/*.*
+mogrify -trim  -resize 250x200 -gravity Center -extent 250x200 -path ./logosChannels/tmp/ ./logosChannels/*.* &&
 echo "Pushing to Github"
-git add --all && git commit -m "Triming picons" && git push
+##git add --all && git commit -m "Triming picons" && git push
 

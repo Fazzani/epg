a7eb942534d76f64ae3c79f45014e180eb11dd67
Fazzani <tunisienheni@outlook.com>
2019-10-27 22:54:51 +0100

    Update update_index.py

--- update_index.py
+++ update_index.py
@@ -2,6 +2,6 @@ import os
 import json
 
 if __name__ == "__main__":
-    d = {"name": [f.lower() for f in os.listdir("./dags")]}
+    d = {"name": [f.lower() for f in os.listdir("./logosChannels")]}
     with open("./index.json", "w+") as f:
         f.write(json.dumps(d))

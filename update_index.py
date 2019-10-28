a8219f1c20466b8b2056684163435ac91c4f9d1d
Fazzani <tunisienheni@gmail.com>
2019-10-28 11:32:47 +0100

    fix

--- update_index.py
+++ update_index.py
@@ -3,5 +3,5 @@ import json
 
 if __name__ == "__main__":
     d = {os.path.splitext(f.lower())[0]:f for f in os.listdir("./logosChannels")}
-    with open("./index.json", "w+") as f:
+    with open("./index_picons.json", "w+") as f:
         f.write(json.dumps(d))

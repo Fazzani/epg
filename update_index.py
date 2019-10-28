700c085fc21463e8cf4f6ac47f53e7ff28dd6b97
Fazzani <tunisienheni@gmail.com>
2019-10-28 11:48:09 +0100

    fix

--- update_index.py
+++ update_index.py
@@ -1,7 +1,11 @@
 import os
 import json
 
+
+PICON_BASE_URL = os.getenv("PICON_BASE_URL", "https://github.com/Fazzani/epg/raw/master/logosChannels/")
+
+
 if __name__ == "__main__":
-    d = {os.path.splitext(f.lower())[0]:f for f in os.listdir("./logosChannels")}
+    d = {os.path.splitext(f.lower())[0]:f"{PICON_BASE_URL}{f}" for f in os.listdir("./logosChannels")}
     with open("./index_picons.json", "w+") as f:
         f.write(json.dumps(d))
